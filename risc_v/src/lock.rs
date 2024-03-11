4acbe51b2680ca675902a6c182a75b1606e6c21c
Minda Chen <akacmd333@gmail.com>
2024-03-11 12:07:48 +0800

    Make fs usable again

--- risc_v/src/lock.rs
+++ risc_v/src/lock.rs
@@ -3,60 +3,68 @@
 // Stephen Marz
 // 26 Apr 2020
 
+use core::{
+    arch::asm,
+    convert::{TryFrom, TryInto},
+};
+
 use crate::syscall::syscall_sleep;
 
 pub const DEFAULT_LOCK_SLEEP: usize = 10000;
 #[repr(u32)]
 pub enum MutexState {
-	Unlocked = 0,
-	Locked = 1
+    Unlocked = 0,
+    Locked = 1,
 }
 
 #[repr(C)]
 pub struct Mutex {
-	state: MutexState
+    state: MutexState,
 }
 
 impl<'a> Mutex {
-	pub const fn new() -> Self {
-		Self { state: MutexState::Unlocked }
-	}
+    pub const fn new() -> Self {
+        Self {
+            state: MutexState::Unlocked,
+        }
+    }
 
-	pub fn val(&'a self) -> &'a MutexState {
-		&self.state
-	}
+    pub fn val(&'a self) -> &'a MutexState {
+        &self.state
+    }
 
-	/// Try to lock the Mutex. If the mutex is already locked, this function returns false, otherwise it will return true if the mutex was acquired.
-	pub fn try_lock(&mut self) -> bool {
-		unsafe {
-			let state: MutexState;
-			llvm_asm!("amoswap.w.aq $0, $1, ($2)\n" : "=r"(state) : "r"(1), "r"(self) :: "volatile");
-			match state {
-				// amoswap returns the OLD state of the lock.  If it was already locked, we didn't acquire it.
-				MutexState::Locked => false,
-				MutexState::Unlocked => true
-			}
-		}
-	}
+    /// Try to lock the Mutex. If the mutex is already locked, this function returns false, otherwise it will return true if the mutex was acquired.
+    pub fn try_lock(&mut self) -> bool {
+        unsafe {
+            let state: u32;
+            asm!("amoswap.w.aq {0}, {1}, ({2})\n", out(reg) state, in(reg) 1, in(reg) self);
+            match state {
+                // amoswap returns the OLD state of the lock.  If it was already locked, we didn't acquire it.
+                1 => false,
+                0 => true,
+                _ => unreachable!(),
+            }
+        }
+    }
 
-	/// Do NOT sleep lock inside of an interrupt context!
-	/// Never use a sleep lock for the process list. Sleeping requires
-	/// the process list to function, so you'll deadlock if you do.
-	pub fn sleep_lock(&mut self) {
-		while !self.try_lock() {
-			syscall_sleep(DEFAULT_LOCK_SLEEP);
-		}
-	}
+    /// Do NOT sleep lock inside of an interrupt context!
+    /// Never use a sleep lock for the process list. Sleeping requires
+    /// the process list to function, so you'll deadlock if you do.
+    pub fn sleep_lock(&mut self) {
+        while !self.try_lock() {
+            syscall_sleep(DEFAULT_LOCK_SLEEP);
+        }
+    }
 
-	/// Can safely be used inside of an interrupt context.
-	pub fn spin_lock(&mut self) {
-		while !self.try_lock() {}
-	}
+    /// Can safely be used inside of an interrupt context.
+    pub fn spin_lock(&mut self) {
+        while !self.try_lock() {}
+    }
 
-	/// Unlock a mutex without regard for its previous state.
-	pub fn unlock(&mut self) {
-		unsafe {
-			llvm_asm!("amoswap.w.rl zero, zero, ($0)" :: "r"(self) :: "volatile");
-		}
-	}
+    /// Unlock a mutex without regard for its previous state.
+    pub fn unlock(&mut self) {
+        unsafe {
+            asm!("amoswap.w.rl zero, zero, ({})", in(reg) self);
+        }
+    }
 }
--- risc_v/src/test.rs
+++ risc_v/src/test.rs
@@ -1,13 +1,50 @@
+use crate::block;
 // test.rs
 use crate::fs::MinixFileSystem;
-use crate::syscall;
+use crate::kmem::{self, kfree, kmalloc};
+use crate::syscall::{syscall_exit, syscall_fs_read};
 /// Test block will load raw binaries into memory to execute them. This function
 /// will load ELF files and try to execute them.
 pub fn test() {
-	// The majority of the testing code needs to move into a system call (execv maybe?)
-	MinixFileSystem::init(8);
-	let path = "/shell\0".as_bytes().as_ptr();
-	syscall::syscall_execv(path,0);
-	println!("I should never get here, execv should destroy our process.");
+    // The majority of the testing code needs to move into a system call (execv maybe?)
+    MinixFileSystem::init(8);
+    test_block_driver();
+    test_read_proc();
+    // 	let path = "/shell\0".as_bytes().as_ptr();
+    // 	syscall::syscall_execv(path,0);
+    // 	println!("I should never get here, execv should destroy our process.");
 }
 
+fn test_read_proc() {
+    let buffer = kmalloc(100);
+    // device, inode, buffer, size, offset
+    let bytes_read = syscall_fs_read(8, 2, buffer, 100, 0);
+    if bytes_read != 9 {
+        println!(
+            "Read {} bytes, but I thought the file was 53 bytes.",
+            bytes_read
+        );
+    } else {
+        for i in 0..9 {
+            print!("{}", unsafe { buffer.add(i).read() as char });
+        }
+        println!();
+    }
+    kfree(buffer);
+    syscall_exit();
+}
+
+fn test_block_driver() {
+    // Let's test the block driver!
+    println!("Testing block driver.");
+    let buffer = kmem::kmalloc(512);
+    block::read(8, buffer, 512, 0x400);
+    for i in 0..48 {
+        print!(" {:02x}", unsafe { buffer.add(i).read() });
+        if 0 == ((i + 1) % 24) {
+            println!();
+        }
+    }
+    kmem::kfree(buffer);
+    println!("Block driver done");
+}
